2beba55cbf2270e31cba91e4b4d6db72c57963e5
Lorenzo Rossi <lorenzorossi@meta.com>
2025-11-10 12:16:56 +0000

    Cleaner, no 3F and getting ready for reserve fixing

--- app.py
+++ app.py
@@ -5,258 +5,29 @@ from ortools.linear_solver import pywraplp
 
 app = Flask(__name__)
 
-input = [
-    {
-        "name": "Fencer1",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 2, "sabre": 1},
-    },
-    {
-        "name": "Fencer2",
-        "gender": "F",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer3",
-        "gender": "F",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-    {
-        "name": "Fencer4",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-    {
-        "name": "Fencer5",
-        "gender": "M",
-        "preference": {"foil": 1, "epee": 3, "sabre": 2},
-    },
-    {
-        "name": "Fencer6",
-        "gender": "F",
-        "preference": {"foil": 1, "epee": 3, "sabre": 2},
-    },
-    {
-        "name": "Fencer7",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 1, "sabre": 2},
-    },
-    {
-        "name": "Fencer8",
-        "gender": "F",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer9",
-        "gender": "F",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer10",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 1, "sabre": 2},
-    },
-    {
-        "name": "Fencer11",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 2, "sabre": 1},
-    },
-    {
-        "name": "Fencer12",
-        "gender": "F",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer13",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-    {
-        "name": "Fencer14",
-        "gender": "F",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer15",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 2, "sabre": 1},
-    },
-    {
-        "name": "Fencer16",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 1, "sabre": 2},
-    },
-    {
-        "name": "Fencer17",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-    {
-        "name": "Fencer18",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 1, "sabre": 2},
-    },
-    {
-        "name": "Fencer19",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 2, "sabre": 1},
-    },
-    {
-        "name": "Fencer20",
-        "gender": "F",
-        "preference": {"foil": 1, "epee": 2, "sabre": 3},
-    },
-    {
-        "name": "Fencer21",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-    {
-        "name": "Fencer22",
-        "gender": "F",
-        "preference": {"foil": 1, "epee": 3, "sabre": 2},
-    },
-    {
-        "name": "Fencer23",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer24",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-    {
-        "name": "Fencer25",
-        "gender": "F",
-        "preference": {"foil": 3, "epee": 2, "sabre": 1},
-    },
-    {
-        "name": "Fencer26",
-        "gender": "F",
-        "preference": {"foil": 3, "epee": 2, "sabre": 1},
-    },
-    {
-        "name": "Fencer27",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer28",
-        "gender": "F",
-        "preference": {"foil": 1, "epee": 3, "sabre": 2},
-    },
-    {
-        "name": "Fencer29",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-    {
-        "name": "Fencer30",
-        "gender": "F",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-    {
-        "name": "Fencer31",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 1, "sabre": 2},
-    },
-    {
-        "name": "Fencer32",
-        "gender": "F",
-        "preference": {"foil": 1, "epee": 3, "sabre": 2},
-    },
-    {
-        "name": "Fencer33",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-    {
-        "name": "Fencer34",
-        "gender": "F",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer35",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer36",
-        "gender": "M",
-        "preference": {"foil": 1, "epee": 2, "sabre": 3},
-    },
-    {
-        "name": "Fencer37",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer38",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-    {
-        "name": "Fencer39",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer40",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 2, "sabre": 1},
-    },
-    {
-        "name": "Fencer41",
-        "gender": "F",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer42",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer43",
-        "gender": "M",
-        "preference": {"foil": 3, "epee": 1, "sabre": 2},
-    },
-    {
-        "name": "Fencer44",
-        "gender": "F",
-        "preference": {"foil": 3, "epee": 2, "sabre": 1},
-    },
-    {
-        "name": "Fencer45",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 3, "sabre": 1},
-    },
-    {
-        "name": "Fencer46",
-        "gender": "M",
-        "preference": {"foil": 2, "epee": 1, "sabre": 3},
-    },
-]
-
 WEAPONS = ["foil", "epee", "sabre"]
-
 
 @app.route("/solve", methods=["POST"])
 def solve():
+    # Get data and calculate M/F particpant numbers
     fencers = request.get_json()["fencers"]
-    # If the number of fencers is not a multiple of 3, randomly exclude male fencers until it is
+    n_f = len([fencer for fencer in fencers if fencer["gender"].upper() == "F"])
+    n_m = len([fencer for fencer in fencers if fencer["gender"].upper() == "M"])
+
+    # If the number of F fencers is not enought to have 1 per team, randomly exclude M fencers until it is
+    if 3 * n_f < len(fencers):
+        to_remove = len(fencers) - 3 * n_f
+        m_fencers = [fencer for fencer in fencers if fencer["gender"].upper() == "M"]
+        reserves = reserves + random.sample(m_fencers, to_remove)
+    fencers = [fencer for fencer in fencers if fencer not in reserves]
+    n_f = len([fencer for fencer in fencers if fencer["gender"].upper() == "F"])
+    n_m = len([fencer for fencer in fencers if fencer["gender"].upper() == "M"])
+
+    # If the number of fencers is not a multiple of 3, randomly exclude M fencers until it is
     if len(fencers) % 3 != 0:
         to_remove = len(fencers) % 3
-        male_fencers = [fencer for fencer in fencers if fencer["gender"].upper() == "M"]
-        reserves = random.sample(male_fencers, to_remove)
-    fencers = [fencer for fencer in fencers if fencer not in reserves]
-
-    # If the number of female fencers is not enought to have 1 per team, randomly exclude male fencers until it is
-    n_females = len([fencer for fencer in fencers if fencer["gender"].upper() == "F"])
-    if 3 * n_females < len(fencers):
-        to_remove = len(fencers) - 3 * n_females
-        male_fencers = [fencer for fencer in fencers if fencer["gender"].upper() == "M"]
-        reserves = reserves + random.sample(male_fencers, to_remove)
+        m_fencers = [fencer for fencer in fencers if fencer["gender"].upper() == "M"]
+        reserves = random.sample(m_fencers, to_remove)
     fencers = [fencer for fencer in fencers if fencer not in reserves]
 
     # Set up the solver
@@ -275,12 +46,12 @@ def solve():
     for i in range(n):
         solver.Add(sum(x[(i, t, w)] for t in range(teams) for w in WEAPONS) == 1)
 
-    # CONSTRAINTS: Each has exactly one of each weapon
+    # CONSTRAINTS: Each team has exactly one of each weapon
     for t in range(teams):
         for w in WEAPONS:
             solver.Add(sum(x[(i, t, w)] for i in range(n)) == 1)
 
-    # CONSTRAINTS: Each team must have at least one female fencer
+    # CONSTRAINTS: Each team must has 1+ F fencer
     for t in range(teams):
         solver.Add(
             sum(
@@ -292,20 +63,28 @@ def solve():
             >= 1
         )
 
-    # OBJECTIVE: maximise preference score
+    # CONSTRAINTS: No team has 3 F fencer
+    for t in range(teams):
+        solver.Add(
+            sum(
+                x[(i, t, w)]
+                for i in range(n)
+                for w in WEAPONS
+                if fencers[i]["gender"].upper() == "F"
+            )
+            < 3
+        )
+
+    # OBJECTIVE: Maximise preference score
     objective = solver.Objective()
     for i in range(n):
         for t in range(teams):
             for w in WEAPONS:
-                pref_rank = fencers[i]["preference"][w]
-                score = 4 - pref_rank
+                score = fencers[i]["preference"][w]
                 objective.SetCoefficient(x[(i, t, w)], score)
     objective.SetMaximization()
 
-    status = solver.Solve()
-    if status != pywraplp.Solver.OPTIMAL:
-        return jsonify({"status": "fail", "teams": {}})
-    # Extract solution
+    # SOLUTION - PART1: Linear optimization
     output = []
     for t in range(teams):
         team_members = {}
@@ -318,6 +97,13 @@ def solve():
                         "preference": fencers[i]["preference"][w],
                     }
         output.append({"team": t + 1, "members": team_members})
+
+    # We might still have some fencers in the reserves to assign
+        # If it's more than 3 then we form teams
+        # If it's less than 3 we assign them to existing teams
+    # if len(reserves) >= 3:
+
+    # Return results
     return jsonify({"status": "ok", "teams": output})
 
 
