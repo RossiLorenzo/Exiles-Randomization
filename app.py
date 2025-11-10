9098431ebe446af4ab4be5977a0062f6e540692e
Lorenzo Rossi <lorenzorossi@meta.com>
2025-11-10 14:08:38 +0000

    First GitHub only push

--- app.py
+++ app.py
@@ -15,25 +15,39 @@ def solve():
     reserves = []
     n_f = len([fencer for fencer in fencers if fencer["gender"].upper() == "F"])
     n_m = len([fencer for fencer in fencers if fencer["gender"].upper() == "M"])
+    n = len(fencers)
+    teams = len(fencers) // 3
 
-    # If the number of F fencers is not enought to have 1 per team, randomly exclude M fencers until it is
-    if 3 * n_f < len(fencers):
-        to_remove = len(fencers) - 3 * n_f
+    # If the number of F is not enought to have 1 per team, randomly exclude M fencers until it is
+    if 3 * n_f < n:
+        to_remove = n - 3 * n_f
         m_fencers = [fencer for fencer in fencers if fencer["gender"].upper() == "M"]
         reserves = reserves + random.sample(m_fencers, to_remove)
-    fencers = [fencer for fencer in fencers if fencer not in reserves]
+        fencers = [fencer for fencer in fencers if fencer not in reserves]
+        n_m = len([fencer for fencer in fencers if fencer["gender"].upper() == "M"])
+        n = len(fencers)
+        teams = len(fencers) // 3
 
-    # If the number of fencers is not a multiple of 3, randomly exclude M fencers until it is
-    if len(fencers) % 3 != 0:
-        to_remove = len(fencers) % 3
+    # I don't think we'll have this, but if we have so many F that we get F only teams remove some F
+    # if n_f > teams*2:
+    #     to_remove = n_f - teams*2
+    #     f_fencers = [fencer for fencer in fencers if fencer["gender"].upper() == "F"]
+    #     reserves = reserves + random.sample(f_fencers, to_remove)
+    #     fencers = [fencer for fencer in fencers if fencer not in reserves]
+    #     n_f = len([fencer for fencer in fencers if fencer["gender"].upper() == "F"])
+    #     n = len(fencers)
+    #     teams = len(fencers) // 3
+
+    # If the number of fencers is not a multiple of 3, randomly exclude fencers until it is
+    if n % 3 != 0:
+        to_remove = n % 3
         m_fencers = [fencer for fencer in fencers if fencer["gender"].upper() == "M"]
         reserves = reserves + random.sample(m_fencers, to_remove)
-    fencers = [fencer for fencer in fencers if fencer not in reserves]
+        fencers = [fencer for fencer in fencers if fencer not in reserves]
+        n = len(fencers)
 
     # Set up the solver
     solver = pywraplp.Solver.CreateSolver("CBC")
-    n = len(fencers)
-    teams = len(fencers) // 3
 
     # VARIABLES: Binary x[i,t,w] = 1 if fencer i is assigned to team t in weapon w, otherwise 0.
     x = {}
@@ -63,7 +77,7 @@ def solve():
             >= 1
         )
 
-    # CONSTRAINTS: Each team has a max of 2 F fencers
+    # CONSTRAINTS: Each team must have a max of 2 F fencers
     for t in range(teams):
         solver.Add(
             sum(
