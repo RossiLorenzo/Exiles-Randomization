131d59e6509e202105c0042e4e333a748575f327
Lorenzo Rossi <lorenzorossi@meta.com>
2025-11-24 11:49:04 +0000

    Add male only teams

--- app.py
+++ app.py
@@ -8,6 +8,82 @@ app = Flask(__name__)
 WEAPONS = ["foil", "epee", "sabre"]
 
 
+def run_solver(fencers, direction=1, all_male=False):
+    # Set up the solver
+    solver = pywraplp.Solver.CreateSolver("CBC")
+    n = len(fencers)
+    teams = n // 3
+
+    # VARIABLES: Binary x[i,t,w] = 1 if fencer i is assigned to team t in weapon w, otherwise 0.
+    x = {}
+    for i in range(n):
+        for t in range(teams):
+            for w in WEAPONS:
+                x[(i, t, w)] = solver.BoolVar(f"x_{i}_{t}_{w}")
+
+    # CONSTRAINTS: Each fencer assigned exactly once
+    for i in range(n):
+        solver.Add(sum(x[(i, t, w)] for t in range(teams) for w in WEAPONS) == 1)
+
+    # CONSTRAINTS: Each team has exactly one of each weapon
+    for t in range(teams):
+        for w in WEAPONS:
+            solver.Add(sum(x[(i, t, w)] for i in range(n)) == 1)
+
+    # CONSTRAINTS: Each team must have 1+ F fencers
+    if not all_male:
+        for t in range(teams):
+            solver.Add(
+                sum(
+                    x[(i, t, w)]
+                    for i in range(n)
+                    for w in WEAPONS
+                    if fencers[i]["category"].upper() == "F"
+                )
+                >= 1
+            )
+
+    # CONSTRAINTS: Each team must have 1+ M fencers
+    for t in range(teams):
+        solver.Add(
+            sum(
+                x[(i, t, w)]
+                for i in range(n)
+                for w in WEAPONS
+                if fencers[i]["category"].upper() == "M"
+            )
+            >= 1
+        )
+
+    # OBJECTIVE: Maximise/Minimize preference score
+    objective = solver.Objective()
+    for i in range(n):
+        for t in range(teams):
+            for w in WEAPONS:
+                if direction == 1:
+                    score = fencers[i]["preference"][w]
+                else:
+                    score = 6 - fencers[i]["preference"][w]
+                objective.SetCoefficient(x[(i, t, w)], score)
+    objective.SetMaximization()
+
+    # SOLUTION - PART1: Linear optimization
+    status = solver.Solve()
+    output = []
+    for t in range(teams):
+        team_members = {}
+        for w in WEAPONS:
+            for i in range(n):
+                if x[(i, t, w)].solution_value() > 0.5:
+                    team_members[w] = {
+                        "name": fencers[i]["name"],
+                        "category": fencers[i]["category"],
+                        "preference": fencers[i]["preference"][w],
+                    }
+        output.append({"team": t + 1, "members": team_members})
+    return output
+
+
 @app.route("/solve", methods=["POST"])
 def solve():
     # Get data and calculate M/F particpant numbers
@@ -18,7 +94,7 @@ def solve():
     print("Participants:", len(fencers))
     print("M:", n_m, " ; F:", n_f)
 
-    # If the number of F is not enought to have 1 per team, randomly exclude M fencers until it is
+    # If the number of F is not enough to have 1 per team, randomly exclude M fencers until it is
     if 3 * n_f < len(fencers):
         to_remove = len(fencers) - 3 * n_f
         m_fencers = [fencer for fencer in fencers if fencer["category"].upper() == "M"]
@@ -43,82 +119,21 @@ def solve():
         fencers = [fencer for fencer in fencers if fencer not in reserves]
 
     # Set up the solver
-    solver = pywraplp.Solver.CreateSolver("CBC")
-    n = len(fencers)
-    teams = n // 3
-    print("Participants:", n, "Teams", teams)
-
-    # VARIABLES: Binary x[i,t,w] = 1 if fencer i is assigned to team t in weapon w, otherwise 0.
-    x = {}
-    for i in range(n):
-        for t in range(teams):
-            for w in WEAPONS:
-                x[(i, t, w)] = solver.BoolVar(f"x_{i}_{t}_{w}")
-
-    # CONSTRAINTS: Each fencer assigned exactly once
-    for i in range(n):
-        solver.Add(sum(x[(i, t, w)] for t in range(teams) for w in WEAPONS) == 1)
-
-    # CONSTRAINTS: Each team has exactly one of each weapon
-    for t in range(teams):
-        for w in WEAPONS:
-            solver.Add(sum(x[(i, t, w)] for i in range(n)) == 1)
-
-    # CONSTRAINTS: Each team must has 1+ F fencers
-    for t in range(teams):
-        solver.Add(
-            sum(
-                x[(i, t, w)]
-                for i in range(n)
-                for w in WEAPONS
-                if fencers[i]["category"].upper() == "F"
-            )
-            >= 1
-        )
-
-    # CONSTRAINTS: Each team must have a max of 2 F fencers
-    for t in range(teams):
-        solver.Add(
-            sum(
-                x[(i, t, w)]
-                for i in range(n)
-                for w in WEAPONS
-                if fencers[i]["category"].upper() == "F"
-            )
-            <= 2
-        )
-
-    # OBJECTIVE: Maximise preference score
-    objective = solver.Objective()
-    for i in range(n):
-        for t in range(teams):
-            for w in WEAPONS:
-                score = fencers[i]["preference"][w]
-                objective.SetCoefficient(x[(i, t, w)], score)
-    objective.SetMaximization()
-
-    # SOLUTION - PART1: Linear optimization
-    status = solver.Solve()
-    output = []
-    for t in range(teams):
-        team_members = {}
-        for w in WEAPONS:
-            for i in range(n):
-                if x[(i, t, w)].solution_value() > 0.5:
-                    team_members[w] = {
-                        "name": fencers[i]["name"],
-                        "category": fencers[i]["category"],
-                        "preference": fencers[i]["preference"][w],
-                    }
-        output.append({"team": t + 1, "members": team_members})
+    output = run_solver(fencers)
 
     # We might still have some fencers in the reserves to assign
     # If it's more than 3 then we form teams
-    # If it's less than 3 we assign them to existing teams
-    # if len(reserves) >= 3:
+    if len(reserves) >= 3:
+        teams = len(reserves) // 3
+        fencers = random.sample(reserves, teams * 3)
+        reserves = [fencer for fencer in reserves if fencer not in fencers]
+        output_reserves = run_solver(fencers, direction=-1, all_male=True)
+        for team in output_reserves:
+            team["team"] = team["team"] + len(output)
+        print(output_reserves)
 
     # Return results
-    return jsonify({"status": "ok", "teams": output})
+    return jsonify({"status": "ok", "teams": output + output_reserves})
 
 
 if __name__ == "__main__":
